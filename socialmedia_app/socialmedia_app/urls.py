0de54a2d436cde2e37dc32a1426c9cd086d9be5e
Zeke Pulido <esp139@msstate.edu>
2023-09-08 23:43:08 -0500

    modified urls for the main app

--- socialmedia_app/socialmedia_app/urls.py
+++ socialmedia_app/socialmedia_app/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('homepage.urls')),
+    path('login/', include('login.urls')),
     path('admin/', admin.site.urls),
 ]
 
