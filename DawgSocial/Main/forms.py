55283be9f76acdaec2ddf280d975d88bef07c988
Zeke Pulido <esp139@msstate.edu>
2023-10-16 15:39:31 -0500

    Modified forms for correct order

--- DawgSocial/Main/forms.py
+++ DawgSocial/Main/forms.py
@@ -3,10 +3,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = {'username','email','first_name', 'last_name','password1','password2'}
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
