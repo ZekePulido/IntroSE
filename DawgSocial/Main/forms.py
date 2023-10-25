3490cdc83042c8f55a32f1ab5ab4c370777b8938
Zeke Pulido <esp139@msstate.edu>
2023-10-24 20:41:45 -0500

    Added a way to Upload Pictures

--- DawgSocial/Main/forms.py
+++ DawgSocial/Main/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import UserProfile
 
 class RegisterForm(UserCreationForm):
     first_name = forms.CharField(required=True)
@@ -10,3 +11,10 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+
+
+class ProfileImageForm(forms.ModelForm):
+    image=forms.ImageField(label="Profile")
+    class Meta:
+        model = UserProfile  
+        fields = ['image']
