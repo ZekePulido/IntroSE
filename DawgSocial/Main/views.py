25ac54d8095909c14f668f9e52648538be7e5d3e
Zeke Pulido <esp139@msstate.edu>
2023-11-15 19:40:35 -0600

    Added the forms back

--- DawgSocial/Main/views.py
+++ DawgSocial/Main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm, ProfileUpdateForm, UpdateUserForm,PostForm, ShareForm
+from .forms import RegisterForm, ProfileUpdateForm, UpdateUserForm,PostForm, ShareForm, LikeForm, DisLikeForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
