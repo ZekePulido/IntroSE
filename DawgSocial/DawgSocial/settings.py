635d386f5b1be8c82d70a8c018585ed2f5e80c00
Zeke Pulido <esp139@msstate.edu>
2023-11-16 20:10:20 -0600

    file

--- DawgSocial/DawgSocial/settings.py
+++ DawgSocial/DawgSocial/settings.py
@@ -95,6 +95,8 @@ DATABASES = {
     }
 }
 
+DATABASES['default']=dj_database_url.parse("redis://red-clbcksent67s73adbdt0:6379")
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
