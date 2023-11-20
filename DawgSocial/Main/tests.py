443f0df50550b937e7e6630046987677f8e48bc3
Zeke Pulido <esp139@msstate.edu>
2023-11-19 19:58:01 -0600

    Added register test:

--- DawgSocial/Main/tests.py
+++ DawgSocial/Main/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.urls import reverse
+from .forms import RegisterForm
 
 class LoginTest(TestCase):
     def setUp(self):
@@ -12,7 +13,7 @@ class LoginTest(TestCase):
         response = self.client.get(reverse('login'))
         self.assertEqual(response.status_code,200)
 
-    def test_login(self):
+    def test_valid_login(self):
         response = self.client.post(reverse('login'), {'username': self.username,'password': self.password})
         self.assertEqual(response.status_code, 302)
         
@@ -24,4 +25,42 @@ class LoginTest(TestCase):
 
         self.assertFalse(response.wsgi_request.user.is_authenticated)
 
-    
+
+class RegisterTest(TestCase):
+    def test_register_view(self):
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_valid_register(self):
+        data = {
+            'username': 'testuser',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'email': 'testuser@example.com',
+            'password1': 'testpassword',
+            'password2': 'testpassword',
+        }
+
+        response = self.client.post(reverse('register'),data)
+        self.assertEqual(response.status_code, 302)
+
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
+
+        self.assertTrue(User.objects.filter(username='testuser').exists())
+
+    def test_invalid_register(self):
+        data = {
+            'username': 'testuser',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'email': 'testuser@example.com',
+            'password1': 'testpassword',
+            'password2': 'differentpassword',
+        }
+
+        response = self.client.post(reverse('register'),data)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
+
+        self.assertFalse(User.objects.filter(username='testuser').exists())
